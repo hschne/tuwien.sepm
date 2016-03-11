6e9bbdae889ed41a8f8450f67d2347eb6064bf57
hschroedl <hansj.schroedl@gmail.com>
2016-03-11 16:41:59 +0100

    Update gitignore, update init script

--- scripts/initialize_database.sql
+++ scripts/initialize_database.sql
@@ -3,12 +3,13 @@ DROP TABLE ARTICLE, RECEIPT, ARTICLE_RECEIPT;
 
 /* Create table 'Article' */
 CREATE TABLE ARTICLE (
-  id int not null AUTO_INCREMENT PRIMARY KEY,
+  id int not null AUTO_INCREMENT,
   name VARCHAR(255) not null,
   price double not null,
   description TEXT not null,
   image_path VARCHAR(255) NOT NULL,
-  category VARCHAR(255) NOT NULL
+  category VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create table 'Receipt' */
@@ -16,17 +17,16 @@ CREATE TABLE RECEIPT(
   ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   creation_date DATETIME not null,
   receiver VARCHAR(255) not null,
-  receiver_adress VARCHAR(255) not null,
+  receiver_adress VARCHAR(255) not null
 );
 
 CREATE TABLE ARTICLE_RECEIPT (
   receipt int not null,
   article int not null,
-  price double not null,
   amount double not null,
   CONSTRAINT RECEIPT_FK FOREIGN KEY (receipt) REFERENCES RECEIPT (id),
   CONSTRAINT ARTICLE_FK FOREIGN KEY (article) REFERENCES ARTICLE (id),
-  primary key (receipt,article),
+  primary key (receipt,article)
 
 );
 
@@ -50,9 +50,9 @@ INSERT INTO RECEIPT (ID, creation_date, receiver, receiver_adress)
 VALUES (2, '2015-12-13 16:11:10', 'Elmer Fudd', 'Oberhausen 9, 23 Jägersdorf');
 
 /* insert dummy data into join table */
-INSERT INTO ARTICLE_RECEIPT (receipt, article, price, amount)
-VALUES (1,3,7,1);
-INSERT INTO ARTICLE_RECEIPT (receipt, article, price, amount)
-VALUES (1,2,2,1);
-INSERT INTO ARTICLE_RECEIPT (receipt, article, price, amount)
-VALUES (2,1,5,4);
+INSERT INTO ARTICLE_RECEIPT (receipt, article, amount)
+VALUES (1,3,1);
+INSERT INTO ARTICLE_RECEIPT (receipt, article, amount)
+VALUES (1,2,1);
+INSERT INTO ARTICLE_RECEIPT (receipt, article, amount)
+VALUES (2,1,4);
