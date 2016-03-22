a701d4d54df7b913f9b712a211ae2f696e46e1ca
hschroedl <hansj.schroedl@gmail.com>
2016-03-22 16:33:53 +0100

    Implement article detail view

--- src/main/java/ui/controller/ArticleDetailsController.java
+++ src/main/java/ui/controller/ArticleDetailsController.java
@@ -1,14 +1,34 @@
 package ui.controller;
 
-import entities.Article;
-import ui.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import ui.model.ArticleModel;
 
 public class ArticleDetailsController extends AbstractController {
 
     private ArticleModel article;
 
+    @FXML
+    private Label name;
+    @FXML
+    private TextField price;
+    @FXML
+    private TextArea description;
+    @FXML
+    private TextField category;
+
     public void setArticle(ArticleModel article) {
         this.article = article;
+        name.setText(article.getName());
+        price.setText(String.valueOf(article.getPrice()));
+        description.setText(article.getDescription());
+        category.setText(article.getCategory());
+    }
+
+    @FXML
+    private void initialize() {
+
     }
 }
--- src/main/java/ui/controller/ArticleOverviewController.java
+++ src/main/java/ui/controller/ArticleOverviewController.java
@@ -1,7 +1,9 @@
 package ui.controller;
 
+import entities.Article;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
@@ -12,8 +14,6 @@ import ui.Main;
 import ui.model.ArticleModel;
 
 public class ArticleOverviewController extends AbstractController {
-
-    private Main mainApp;
 
     @FXML
     private TableView<ArticleModel> articleTable;
@@ -45,12 +45,34 @@ public class ArticleOverviewController extends AbstractController {
     @FXML
     private void initialize() {
         // Initialize the person table with the two columns.
+
+        initializeDoubleClick();
+
         nameColumn.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
         priceColumn.setCellValueFactory(cellData -> cellData.getValue().getPriceProperty().asObject());
         descriptionColumn.setCellValueFactory(cellData -> cellData.getValue().getDescriptionProperty());
         categoryColumn.setCellValueFactory(cellData -> cellData.getValue().getCategoryProperty());
 
     }
+
+    private void initializeDoubleClick() {
+        articleTable.setRowFactory( tv -> {
+            TableRow<ArticleModel> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (! row.isEmpty()) ) {
+                    ArticleModel rowData = row.getItem();
+                    showArticleDetails(rowData);
+                }
+            });
+            return row ;
+        });
+    }
+
+    private void showArticleDetails(ArticleModel article){
+        mainApp.showArticleDetails(article);
+    }
+
+
 
 
 }
